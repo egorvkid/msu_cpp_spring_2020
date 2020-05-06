5f0878f4ddf387c96e05dc469784b3bd584461f5
EgorKlimov <kes17.99@mail.ru>
2020-05-06 04:36:07 +0300

    Add files via upload

---
+++ 08/ThreadPool.cpp
@@ -0,0 +1,44 @@
+#include "ThreadPool.h"
+
+ThreadPool::ThreadPool(size_t poolSize) : poolSize(poolSize) {
+	stop_ = false;
+	// initialize threads
+	for (size_t i = 0; i < poolSize; i++)
+		threads_.push_back(std::thread(&ThreadPool::initialize_thread, this));
+}
+
+void ThreadPool::initialize_thread() {
+	while (true) {
+		std::function<void()> job;
+
+		{
+			// set the lock
+			std::unique_lock <std::mutex> lock(mutex_);
+
+			// wait if there's no jobs and pool is not stopped
+			while (!stop_ && jobs_.empty())
+				condition_.wait(lock);
+
+			// if pool is stopped - stop execution
+			if (stop_) break;
+
+			// extract the job from the queue
+			job = jobs_.front();
+			jobs_.pop();
+		}
+
+		job(); // exec the job
+	}
+	return;
+}
+
+
+ThreadPool::~ThreadPool()
+{
+	stop_ = true;
+	condition_.notify_all();
+
+	for (std::thread& thread_ : threads_) {
+		thread_.join();
+	}
+}
